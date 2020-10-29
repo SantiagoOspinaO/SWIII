fafc9a22d565fb2d2263709aecd37921ba7038de
Santiago Ospina <santiago.m200@outlook.es>
2020-10-29 14:13:18 -0500

    update HistoricoTemperatura

--- src/main/java/co/com/swiii/registrotemperatura/service/historicotemperatura/implement/HistoricoTemperaturaServiceImpl.java
+++ src/main/java/co/com/swiii/registrotemperatura/service/historicotemperatura/implement/HistoricoTemperaturaServiceImpl.java
@@ -1,8 +1,8 @@
 package co.com.swiii.registrotemperatura.service.historicotemperatura.implement;
 
-import co.com.swiii.registrotemperatura.converter.EmpleadoConverter;
-import co.com.swiii.registrotemperatura.model.Empleado;
-import co.com.swiii.registrotemperatura.repository.EmpleadoRepository;
+import co.com.swiii.registrotemperatura.converter.HistoricoTemperaturaConverter;
+import co.com.swiii.registrotemperatura.model.HistoricoTemperatura;
+import co.com.swiii.registrotemperatura.repository.HistoricoTemperaturaRepository;
 import co.com.swiii.registrotemperatura.service.historicotemperatura.HistoricoTemperaturaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,27 +13,27 @@ import java.util.List;
 public class HistoricoTemperaturaServiceImpl implements HistoricoTemperaturaService {
 
     @Autowired
-    private EmpleadoConverter empleadoConverter;
+    private HistoricoTemperaturaConverter historicoTemperaturaConverter;
     @Autowired
-    private EmpleadoRepository empleadoRepository;
+    private HistoricoTemperaturaRepository historicoTemperaturaRepository;
 
     @Override
-    public void save(Empleado empleado) {
-        empleadoRepository.save(empleadoConverter.modelToEntity(empleado));
+    public void save(HistoricoTemperatura historicoTemperatura) {
+        historicoTemperaturaRepository.save(historicoTemperaturaConverter.modelToEntity(historicoTemperatura));
     }
 
     @Override
-    public Empleado findById(Long id) {
-        return empleadoConverter.entityToModel(empleadoRepository.findById(id));
+    public HistoricoTemperatura findById(Long id) {
+        return historicoTemperaturaConverter.entityToModel(historicoTemperaturaRepository.findById(id));
     }
 
     @Override
-    public List<Empleado> findAll() {
-        return empleadoConverter.entityToModel(empleadoRepository.findAll());
+    public List<HistoricoTemperatura> findAll() {
+        return historicoTemperaturaConverter.entityToModel(historicoTemperaturaRepository.findAll());
     }
 
     @Override
     public void deleteById(Long id) {
-        empleadoRepository.deleteById(id);
+        historicoTemperaturaRepository.deleteById(id);
     }
 }
